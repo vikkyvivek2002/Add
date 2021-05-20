56f7f33873c802392756327576d0c3c6b963e33f
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-20 20:22:34 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,8 @@
+class Add
+{
+    public static void main(String[] args)
+          { int x=5, y=6,sum;
+           sum=x+y;
+           
+           System.out.println("Sum of these numbers: "+sum);
+} }
